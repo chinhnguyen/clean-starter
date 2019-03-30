2ac2e60c01528111bcbd7847a8cb78cad5791896
Chinh Nguyen <chinh@smarp.com>
2019-03-30 11:46:43 +0200

    new jwt config correct build error

--- app/lib/infrastructure/webserver/middlewares/AuthMiddleware.ts
+++ app/lib/infrastructure/webserver/middlewares/AuthMiddleware.ts
@@ -13,9 +13,6 @@ import AuthController from "../../../interfaces/controllers/AuthController";
  */
 export function authorizeUser(roles: string[]): RequestHandler {
   return async (request: Request, response: Response, next: NextFunction): Promise<void> => {
-    // let token = request.param("token")
-    // console.log('token-param', token)
-    // if (isEmpty(token)) {    
     const bearerHeader = request.header('authorization')
     if (isEmpty(bearerHeader)) {
       response.sendProblem(new ForbiddenProblem({
@@ -25,10 +22,8 @@ export function authorizeUser(roles: string[]): RequestHandler {
     }
     const bearer = bearerHeader.split(' ')
     const token = bearer[1]
-    // console.log('token-header', token)
-    // }
     const authController = resolve<AuthController>('authController')
-    const resolvedToken = await authController.resolveToken(token, roles)
+    const resolvedToken = await authController.resolveToken(token)
     if (resolvedToken instanceof Problem) {
       response.sendProblem(resolvedToken)
       return
